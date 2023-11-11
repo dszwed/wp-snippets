74039c3860bbeaa50bbec1754f0ddabd12a1f5d4
Dawid <dawid.szwed@gmail.com>
2023-11-11 16:01:27 +0100

    Create custom-tag-validation.php

---
+++ custom-tag-validation.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  Validate if my custom field is populated
+ */
+add_filter( 'wpcf7_validate_my_custom_tag*', 'wp360_my_custom_tag_validation_handler'], 10, 2 );
+
+/**
+ * @param \WPCF7_Validation $result
+ * @param \WPCF7_FormTag $tag
+ * @return \WPCF7_Validation
+ */
+function wp360_my_custom_tag_validation_handler($result, $tag): \WPCF7_Validation {
+  $tag = new \WPCF7_FormTag( $tag );
+
+  $name = $tag->name;
+
+  //get value filled by the user
+  $value = filter_input( INPUT_POST, $name, FILTER_VALIDATE_INT );
+
+  if ( $tag->is_required() && !$value ) {
+      $result->invalidate( $tag, wpcf7_get_message( 'invalid_required' ) );
+  }
+
+  return $result;
+}
