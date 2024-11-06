27c83194972f4b87e4b55eff3c891b3de34a0c70
Dawid <dawid.szwed@gmail.com>
2024-11-06 22:01:04 +0100

    Update content-security-policy-reporting.php

--- headers/content-security-policy-reporting.php
+++ headers/content-security-policy-reporting.php
@@ -1,1 +1,8 @@
+/**
+ * Set header for Content-Security-Policy only for reporting
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/report-to
+ * Send request is a POST
+ */
+
+header("Content-Security-Policy-Report-Only: default-src 'none'; report-uri https://example.com/csp-report");
 
